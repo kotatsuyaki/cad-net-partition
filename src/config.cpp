4597c72c7fd8fd8888cefce73c25c20556a75d73
kotatsuyaki <robinhuang123@gmail.com>
2022-05-18 10:12:50 +0800

    Remove obsolete config options

--- src/config.cpp
+++ src/config.cpp
@@ -6,11 +6,6 @@
 #include <cstdlib>
 
 Config::Config() {
-  if (std::getenv("PA2_DEBUG_MOVES")) {
-    fmt::print("PA2_DEBUG_MOVES is set\n");
-    debug_moves = true;
-  }
-
   if (std::getenv("PA2_DEBUG_INPUTS")) {
     fmt::print("PA2_DEBUG_INPUTS is set\n");
     debug_inputs = true;
@@ -19,20 +14,5 @@ Config::Config() {
   if (std::getenv("PA2_VERIFY_BLOCKS")) {
     fmt::print("PA2_VERIFY_BLOCKS is set\n");
     verity_blocks = true;
-  }
-
-  if (std::getenv("PA2_ALLOW_KWAY")) {
-    fmt::print("PA2_ALLOW_KWAY is set\n");
-    allow_kway = true;
-  }
-
-  if (auto cstr = std::getenv("PA2_PASS_ROUNDS")) {
-    fmt::print("PA2_PASS_ROUNDS is set\n");
-    std::string str{cstr};
-    try {
-      pass_rounds = std::stoi(str);
-    } catch (const std::exception& e) {
-      fmt::print("Failed to parse PA2_PASS_ROUNDS: {}\n", e.what());
-    }
   }
 }
--- src/config.hpp
+++ src/config.hpp
@@ -24,20 +24,11 @@ struct Config {
   // Constructs a `Config` from environment variables.
   Config();
 
-  // Whether to print detailed debug information about moves during passes.
-  bool debug_moves = false;
-
   // Whether to print inputs upon start.
   bool debug_inputs = false;
 
-  // Rounds of passes to perform.
-  int pass_rounds = default_rounds;
-
   // Whether to verify partitions.
   bool verity_blocks = false;
-
-  // Allow k-way (k > 2) partitioning.
-  bool allow_kway = false;
 };
 
 #endif  // CONFIG_HPP_
